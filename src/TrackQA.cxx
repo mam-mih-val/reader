4a63a18ad7948eb16d087f5c4ab0f0e0649d88ab
mam-mih-val <mam.mih.val@gmail.com>
2019-02-27 11:59:27 +0300

    Hot fix

--- src/TrackQA.cxx
+++ src/TrackQA.cxx
@@ -16,8 +16,8 @@ void TrackQA::InitHistograms()
 	vHisto1D[massTOF_selected] =	new TH1F("massTOF_selected",";m^{2} selected, [#frac{GeV}{c^{2}}];counts",100,-0.2,4);
 	vHisto1D[rapidityMDC] =		 new TH1F("rapidityMDC",";rapidity, y;counts",100,-1,2);
 	vHisto1D[rapidityMDC_selected]= new TH1F("rapidityMDC_selected",";rapidity selected, y;counts",100,-1,2);
-	vHisto1D[pseudorapidityMDC] =   new TH1F("pseudorapidityMDC",";pseudorapidity;counts",100,-1.5,1.5);
-	vHisto1D[pseudorapidityMDC_selected]=new TH1F("pseudorapidityMDC_selected",";pseudorapidity_selected;counts",100,-1.5,1.5);
+	vHisto1D[pseudorapidityMDC] =   new TH1F("pseudorapidityMDC",";pseudorapidity;counts",100,0,3);
+	vHisto1D[pseudorapidityMDC_selected]=new TH1F("pseudorapidityMDC_selected",";pseudorapidity_selected;counts",100,0,3);
 	vHisto1D[phiMDC] =			  new TH1F("phiMDC",";phi;counts",100,-3.1415,3.1415);
 	vHisto1D[phiMDC_selected] =	 new TH1F("phiMDC_selected",";#phi selected;counts",100,-3.1415,3.1415);
 	vHisto1D[betaTOF] =			 new TH1F("betaTOF",";#beta;counts",100,0,1.2);
@@ -29,12 +29,12 @@ void TrackQA::InitHistograms()
 	vHisto2D[phi_pt_selected]  =	new TH2F("phi&pt_selected",";pt selected, [GeV/c];phi selected, [rad]",100,0,2,100,-3.14,3.14);
 	vHisto2D[pt_rapidity]  =		new TH2F("pt&rapidity",";rapidity;pt, [GeV/c]",100,-1,1,100,0,2);
 	vHisto2D[pt_rapidity_selected]= new TH2F("pt&rapidity_selected",";rapidity selected;pt selected, [GeV/c]",100,-1,1,100,0,2);
-	vHisto2D[phi_pseudorapidity]  = new TH2F("phi&pseudorapidity",";pseudorapidity;phi, [rad]",100,-1,1,100,-3.14,3.14);
-	vHisto2D[phi_pseudorapidity_selected]  = new TH2F("phi&pseudorapidity_selected",";pseudorapidity_selected;phi_selected, [rad]",100,-1,1,100,-3.14,3.14);
-	vHisto2D[pt_pseudorapidity]  =  new TH2F("pt&pseudorapidity",";pseudorapidity;pt, [GeV/c]",100,-1,1,100,0,2);
-	vHisto2D[pt_pseudorapidity_selected]=new TH2F("pt&pseudorapidity_selected",";pseudorapidity selected;pt selected, [GeV/c]",100,-1,1,100,0,2);
-	vHisto2D[rapidity_pseudorapidity]=new TH2F("rapidity&pseudorapidity",";pseudorapidity;rapidity",100,-1,1,100,-1,1);
-	vHisto2D[rapidity_pseudorapidity_selected]=new TH2F("rapidity&pseudorapidity_selected",";pseudorapidity selected;rapidity selected",100,-1,1,100,-1,1);
+	vHisto2D[phi_pseudorapidity]  = new TH2F("phi&pseudorapidity",";pseudorapidity;phi, [rad]",100,0,3,100,-3.14,3.14);
+	vHisto2D[phi_pseudorapidity_selected]  = new TH2F("phi&pseudorapidity_selected",";pseudorapidity_selected;phi_selected, [rad]",100,0,3,100,-3.14,3.14);
+	vHisto2D[pt_pseudorapidity]  =  new TH2F("pt&pseudorapidity",";pseudorapidity;pt, [GeV/c]",,100,0,3,100,0,2);
+	vHisto2D[pt_pseudorapidity_selected]=new TH2F("pt&pseudorapidity_selected",";pseudorapidity selected;pt selected, [GeV/c]",100,0,3,100,0,2);
+	vHisto2D[rapidity_pseudorapidity]=new TH2F("rapidity&pseudorapidity",";pseudorapidity;rapidity",,100,0,3,100,-1,1);
+	vHisto2D[rapidity_pseudorapidity_selected]=new TH2F("rapidity&pseudorapidity_selected",";pseudorapidity selected;rapidity selected",,100,0,3,100,-1,1);
 	vHisto2D[dEdXTOF_p] =			  new TH2F("TOF_dE/dx&p",";p, [GeV/c]; #frac{dE}{dx}",100,0,2.5,100,0,100);
 	vHisto2D[dEdXTOF_p_selected] =	 new TH2F("TOF_dE/dx&p_selected",";p selected, [GeV/c]; #frac{dE}{dx} selected",100,0,2.5,100,0,100);
 	vHisto2D[dEdXMDC_p] =			  new TH2F("MDC_dE/dx&p",";p, [GeV/c]; #frac{dE}{dx}",100,0,2.5,100,0,100);
@@ -74,13 +74,14 @@ void TrackQA::FillHistograms(DataTreeEvent* fEvent)
 		vHisto2D[dEdXMDC_p]->Fill( fMomentum.P(),fTrack->GetdEdx(HADES_constants::kMDC_all) );
 		vHisto2D[dEdXTOF_p]->Fill( fMomentum.P(),fTrack->GetdEdx(HADES_constants::kMETA) );
 		vHisto2D[beta_p]->Fill(fMomentum.P(),fBeta);
+		float fPR = fMomentum.PseudoRapidity();
 		fMomentum.Boost(b);
 		vHisto1D[rapidityMDC]->Fill(fMomentum.Rapidity());
 		vHisto1D[phiMDC]->Fill(fMomentum.Phi());
 		vHisto2D[phi_rapidity]->Fill(fMomentum.Rapidity(),fMomentum.Phi());
 		vHisto2D[phi_pt]->Fill(fMomentum.Pt(),fMomentum.Phi());
 		vHisto2D[pt_rapidity]->Fill(fMomentum.Rapidity(),fMomentum.Pt());
-		vHisto2D[rapidity_pseudorapidity]->Fill(fMomentum.PseudoRapidity(),fMomentum.Rapidity());
+		vHisto2D[rapidity_pseudorapidity]->Fill(fPR,fMomentum.Rapidity());
 
 		if ( !fSelector.IsCorrectEvent(fEvent) || !fSelector.IsCorrectTrack(i) )
 			continue;
@@ -96,12 +97,12 @@ void TrackQA::FillHistograms(DataTreeEvent* fEvent)
 		vHisto2D[dEdXTOF_p_selected]->Fill( fMomentum.P(),fTrack->GetdEdx(HADES_constants::kMETA) );
 		vHisto2D[beta_p_selected]->Fill(fMomentum.P(),fBeta);
 		fMomentum.Boost(b);
-		vHisto1D[rapidityMDC]->Fill(fMomentum.Rapidity());
-		vHisto1D[phiMDC]->Fill(fMomentum.Phi());
+		vHisto1D[rapidityMDC_selected]->Fill(fMomentum.Rapidity());
+		vHisto1D[phiMDC_selected]->Fill(fMomentum.Phi());
 		vHisto2D[phi_rapidity]->Fill(fMomentum.Rapidity(),fMomentum.Phi());
 		vHisto2D[phi_pt]->Fill(fMomentum.Pt(),fMomentum.Phi());
 		vHisto2D[pt_rapidity]->Fill(fMomentum.Rapidity(),fMomentum.Pt());
-		vHisto2D[rapidity_pseudorapidity]->Fill(fMomentum.PseudoRapidity(),fMomentum.Rapidity());
+		vHisto2D[rapidity_pseudorapidity]->Fill(fPR,fMomentum.Rapidity());
 	}
 }
 
@@ -168,43 +169,43 @@ void TrackQA::SaveHistograms(TString PicName)
 	legend->Draw();
 //	***
 	vCanvas[phi_kinematics]->cd(1)->SetLogz();
-	vHisto1D[phi_rapidity]->Draw("colz");
+	vHisto2D[phi_rapidity]->Draw("colz");
 	vCanvas[phi_kinematics]->cd(2)->SetLogz();
-	vHisto1D[phi_pseudorapidity]->Draw("colz");
+	vHisto2D[phi_pseudorapidity]->Draw("colz");
 	vCanvas[phi_kinematics]->cd(3)->SetLogz();
-	vHisto1D[phi_pt]->Draw("colz");
+	vHisto2D[phi_pt]->Draw("colz");
 	vCanvas[phi_kinematics]->cd(4)->SetLogz();
-	vHisto1D[phi_rapidity_selected]->Draw("colz");
+	vHisto2D[phi_rapidity_selected]->Draw("colz");
 	vCanvas[phi_kinematics]->cd(5)->SetLogz();
-	vHisto1D[phi_pseudorapidity_selected]->Draw("colz");
+	vHisto2D[phi_pseudorapidity_selected]->Draw("colz");
 	vCanvas[phi_kinematics]->cd(6)->SetLogz();
-	vHisto1D[phi_pt_selected]->Draw("colz");
+	vHisto2D[phi_pt_selected]->Draw("colz");
 //	***
 	vCanvas[pt_kinematics]->cd(1)->SetLogz();
-	vHisto1D[pt_rapidity]->Draw("colz");
+	vHisto2D[pt_rapidity]->Draw("colz");
 	vCanvas[pt_kinematics]->cd(2)->SetLogz();
-	vHisto1D[pt_pseudorapidity]->Draw("colz");
+	vHisto2D[pt_pseudorapidity]->Draw("colz");
 	vCanvas[pt_kinematics]->cd(3)->SetLogz();
-	vHisto1D[rapidity_pseudorapidity]->Draw("colz");
+	vHisto2D[rapidity_pseudorapidity]->Draw("colz");
 	vCanvas[pt_kinematics]->cd(4)->SetLogz();
-	vHisto1D[pt_rapidity_selected]->Draw("colz");
+	vHisto2D[pt_rapidity_selected]->Draw("colz");
 	vCanvas[pt_kinematics]->cd(5)->SetLogz();
-	vHisto1D[pt_pseudorapidity_selected]->Draw("colz");
+	vHisto2D[pt_pseudorapidity_selected]->Draw("colz");
 	vCanvas[pt_kinematics]->cd(6)->SetLogz();
-	vHisto1D[rapidity_pseudorapidity_selected]->Draw("colz");
+	vHisto2D[rapidity_pseudorapidity_selected]->Draw("colz");
 //	***
 	vCanvas[mass_qa]->cd(1);
-	vHisto1D[dEdXTOF_p]->Draw("colz");
+	vHisto2D[dEdXTOF_p]->Draw("colz");
 	vCanvas[mass_qa]->cd(2);
-	vHisto1D[dEdXMDC_p]->Draw("colz");
+	vHisto2D[dEdXMDC_p]->Draw("colz");
 	vCanvas[mass_qa]->cd(3);
-	vHisto1D[beta_p]->Draw("colz");
+	vHisto2D[beta_p]->Draw("colz");
 	vCanvas[mass_qa]->cd(4);
-	vHisto1D[dEdXTOF_p_selected]->Draw("colz");
+	vHisto2D[dEdXTOF_p_selected]->Draw("colz");
 	vCanvas[mass_qa]->cd(5);
-	vHisto1D[dEdXMDC_p_selected]->Draw("colz");
+	vHisto2D[dEdXMDC_p_selected]->Draw("colz");
 	vCanvas[mass_qa]->cd(6);
-	vHisto1D[beta_p_selected]->Draw("colz");
+	vHisto2D[beta_p_selected]->Draw("colz");
 // ***
 	for(int i=0; i<NumCanvases; i++)
     {
