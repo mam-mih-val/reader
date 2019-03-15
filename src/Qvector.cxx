212e38a3255279dc67119526c61fab71e00a556c
mam-mih-val <mam.mih.val@gmail.com>
2019-03-15 18:27:27 +0300

    Q-vectors debug

--- src/Qvector.cxx
+++ src/Qvector.cxx
@@ -15,25 +15,25 @@ Qvector::Qvector()
 void Qvector::InitHistograms()
 {
 	cout << "Initialization of Q-vectors histograms" << endl;
-    vProfile[meanQx1] =         		new TProfile("MeanQx vs Centrality subevent 1",";centrality;mean Qx",10,0,50);
-    vProfile[meanQy1] =         		new TProfile("MeanQy vs Centrality subevent 1",";centrality;mean Qy",10,0,50);
-	vProfile[meanQx2] =         		new TProfile("MeanQx vs Centrality subevent 2",";centrality;mean Qx",10,0,50);
-    vProfile[meanQy2] =         		new TProfile("MeanQy vs Centrality subevent 2",";centrality;mean Qy",10,0,50);
-    vProfile[resolution] =     			new TProfile("Resolution vs Centrality",";centrality;R_{1}",10,0,50);
+    vProfile[meanQx1] =         		new TProfile("MeanQx vs Centrality subevent 1",";centrality;mean Qx",10,0.,50.);
+    vProfile[meanQy1] =         		new TProfile("MeanQy vs Centrality subevent 1",";centrality;mean Qy",10,0.,50.);
+	vProfile[meanQx2] =         		new TProfile("MeanQx vs Centrality subevent 2",";centrality;mean Qx",10,0.,50.);
+    vProfile[meanQy2] =         		new TProfile("MeanQy vs Centrality subevent 2",";centrality;mean Qy",10,0.,50.);
+    vProfile[resolution] =     			new TProfile("Resolution vs Centrality",";centrality;R_{1}",10,0.,50.);
 
     vHisto1D[QxNotRecentred1] =       	new TH1F("QxNotRecrntred subevent 1",";Qx;counts",100,-1,1);
     vHisto1D[QyNotRecentred1] =       	new TH1F("QyNotRecentred subevent 1",";Qy;counts",100,-1,1);
     vHisto1D[QxRecentred1] =          	new TH1F("QxRecentred subevent 1",";Qx recentred;couts",100,-1,1);
     vHisto1D[QyRecentred1] =          	new TH1F("QyRecrntred subevent 1",";Qy recentred;counts",100,-1,1);
 	vHisto1D[PsiEPNotRecentred1]=		new TH1F("PsiEPNotRecentred subevent 1",";#PsiEP [rad];counts",100,-3.15,3.15);
-	vHisto1D[PsiEPRecentred1]=			new TH1F("PsiEPNotRecentred subevent 1",";#PsiEP recentred [rad];counts",100,-3.15,3.15);
+	vHisto1D[PsiEPRecentred1]=			new TH1F("PsiEPRecentred subevent 1",";#PsiEP recentred [rad];counts",100,-3.15,3.15);
 	
 	vHisto1D[QxNotRecentred2] =       	new TH1F("QxNotRecrntred subevent 2",";Qx;counts",100,-1,1);
     vHisto1D[QyNotRecentred2] =       	new TH1F("QyNotRecentred subevent 2",";Qy;counts",100,-1,1);
     vHisto1D[QxRecentred2] =          	new TH1F("QxRecentred subevent 2",";Qx recentred;couts",100,-1,1);
     vHisto1D[QyRecentred2] =          	new TH1F("QyRecrntred subevent 2",";Qy recentred;counts",100,-1,1);
 	vHisto1D[PsiEPNotRecentred2]=		new TH1F("PsiEPNotRecentred subevent 2",";#PsiEP [rad];counts",100,-3.15,3.15);
-	vHisto1D[PsiEPRecentred2]=			new TH1F("PsiEPNotRecentred subevent 2",";#PsiEP recentred [rad];counts",100,-3.15,3.15);
+	vHisto1D[PsiEPRecentred2]=			new TH1F("PsiEPRecentred subevent 2",";#PsiEP recentred [rad];counts",100,-3.15,3.15);
 }
 
 void Qvector::FillCorrections(DataTreeEvent* fEvent)
@@ -77,10 +77,15 @@ void Qvector::FillCorrections(DataTreeEvent* fEvent)
 		}
 	}
 	Float_t fCentrality = fEvent->GetCentrality();
-	vProfile[meanQx1]->Fill(fCentrality,fQ[0][0]);
-	vProfile[meanQx2]->Fill(fCentrality,fQ[0][1]);
-	vProfile[meanQy1]->Fill(fCentrality,fQ[1][0]);
-	vProfile[meanQy2]->Fill(fCentrality,fQ[1][1]);
+
+	if ( fQ[0][0] == fQ[0][0] )
+		vProfile[meanQx1]->Fill(fCentrality, fQ[0][0]);
+	if ( fQ[0][1] == fQ[0][1] )
+		vProfile[meanQx2]->Fill(fCentrality, fQ[0][1]);
+	if ( fQ[1][0] == fQ[1][0] )
+		vProfile[meanQy1]->Fill(fCentrality, fQ[1][0]);
+	if ( fQ[1][1] == fQ[1][1] )
+		vProfile[meanQy2]->Fill(fCentrality, fQ[1][1]);
 
 	vHisto1D[QxNotRecentred1]->Fill(fQ[0][0]);
 	vHisto1D[QyNotRecentred1]->Fill(fQ[1][0]);
@@ -131,13 +136,12 @@ void Qvector::Estimate(DataTreeEvent* fEvent)
 		}
 	}
 	Float_t fCentrality = fEvent->GetCentrality();
-	//cout << fCentrality << endl;
 	int iCentralityBin = fCentrality/5+1;
+	//cout << vProfile[meanQx1]->GetBinContent( iCentralityBin ) << endl;
 	fQ[0][0] -= vProfile[meanQx1]->GetBinContent( iCentralityBin );
 	fQ[0][1] -= vProfile[meanQx2]->GetBinContent( iCentralityBin );
 	fQ[1][0] -= vProfile[meanQy1]->GetBinContent( iCentralityBin );
 	fQ[1][1] -= vProfile[meanQy2]->GetBinContent( iCentralityBin );
-
 	vHisto1D[QxRecentred1]->Fill( fQ[0][0] );
 	vHisto1D[QyRecentred1]->Fill( fQ[1][0] );
 	vHisto1D[QxRecentred2]->Fill( fQ[0][1] );
@@ -216,18 +220,34 @@ void Qvector::SaveHistograms(TString sPicName)
 	vCanvas[QvectorsDistribution]->SaveAs("../histograms/"+sPicName+".png");
 
 	vCanvas[MeanQvectors] = new TCanvas("Canvas2","Qvectors",4000,2500);
-	vCanvas[MeanQvectors]->Divide(3,2,0.005,0.0001);
+	vCanvas[MeanQvectors]->Divide(2,2,0.005,0.0001);
 
 	vCanvas[MeanQvectors]->cd(1);
+	vProfile[meanQx1]->SetMarkerStyle(20);
+	vProfile[meanQx1]->SetMarkerSize(4);
+	vProfile[meanQx1]->SetLineWidth(4);
+	vProfile[meanQx1]->SetLineColor(1);
 	vProfile[meanQx1]->Draw();
 
 	vCanvas[MeanQvectors]->cd(2);
+	vProfile[meanQx2]->SetMarkerStyle(20);
+	vProfile[meanQx2]->SetMarkerSize(4);
+	vProfile[meanQx2]->SetLineWidth(4);
+	vProfile[meanQx2]->SetLineColor(1);
 	vProfile[meanQx2]->Draw();
 
 	vCanvas[MeanQvectors]->cd(3);
+	vProfile[meanQy1]->SetMarkerStyle(20);
+	vProfile[meanQy1]->SetMarkerSize(4);
+	vProfile[meanQy1]->SetLineWidth(4);
+	vProfile[meanQy1]->SetLineColor(1);
 	vProfile[meanQy1]->Draw();
 
-	vCanvas[MeanQvectors]->cd(3);
+	vCanvas[MeanQvectors]->cd(4);
+	vProfile[meanQy2]->SetMarkerStyle(20);
+	vProfile[meanQy2]->SetMarkerSize(4);
+	vProfile[meanQy2]->SetLineWidth(4);
+	vProfile[meanQy2]->SetLineColor(1);
 	vProfile[meanQy2]->Draw();
 
 	vCanvas[MeanQvectors]->SaveAs("../histograms/"+sPicName+"_1.png");
