8cfe85b2b76db9519324c177cda88b9e99f13876
mam-mih-val <mam.mih.val@gmail.com>
2019-05-15 02:36:56 +0300

    Batch scripts added

---
+++ batch/DT_Reader_batch.sh
@@ -0,0 +1,30 @@
+hadesroot=/cvmfs/hades.gsi.de/install/6.12.06/hydra2-4.9w/defall.sh 
+
+current_dir = $(pwd)
+partition = main
+time=8:00:00
+executable=../build/DT_Reader
+
+file_list =     ${1}
+cmd =           ${2}
+config =        ${3}
+output_dir =    ${4}
+
+log_dir=${output_dir}/log
+
+mkdir -p $output_dir
+mkdir -p $log_dir
+
+n_runs=$(cat file_list | wc -l)
+
+job_range=1-$n_runs
+
+executable=${current_dir}/../build/DT_Reader
+
+echo executable=$executable
+echo output_dir=$output_dir
+echo log_dir=$log_dir
+echo n_runs=$n_runs
+echo job_range=$job_range
+
+sbatch -J DT_Reader -p $partition -t $time -a $job_range -e ${log_dir}/%A_%a.e -o ${log_dir}/%A_%a.o --export=executable=$executable,output_dir=$output_dir,file_list=$file_list,hadesroot=$hadesroot,cmd=$cmd,config=$config batch_run.sh
---
+++ batch/batch_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+format='+%Y/%m/%d-%H:%M:%S'
+
+date $format
+
+job_num=$(($SLURM_ARRAY_TASK_ID))
+output_file=$output_dir/$job_num.root
+input_file = sed "${job_num}q;d" file_list
+
+echo "loading " $hadesroot
+source $hadesroot
+echo "executing $executable  $input_files $output_file $n_events"
+$executable  $input_file $cmd $config $output_file
+
+echo JOB FINISHED!
+date $format
