248dd5be89201e125b6afc4e9b1607dcefd9268a
mam-mih-val <mam.mih.val@gmail.com>
2019-05-15 18:49:38 +0300

    Bugs fixed

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -8,8 +8,8 @@ job_num=$(($SLURM_ARRAY_TASK_ID))
 output_file=$output_dir/$job_num.root
 input_file=`sed -n "${job_num}p" < $file_list`
 
-#echo "loading " $hadesroot
-#source $hadesroot
+echo "loading " $hadesroot
+source $hadesroot
 echo "executing $executable  $input_files $output_file $n_events"
 $executable $input_file $cmd $config $output_file
 
