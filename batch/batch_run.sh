abf2f2b6b6f5e736c5a891b024bc363e9a4a2fce
mam-mih-val <mam.mih.val@gmail.com>
2019-06-10 19:22:21 +0300

    Bugs fixed

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -5,7 +5,7 @@ format='+%Y/%m/%d-%H:%M:%S'
 date $format
 
 job_num=$(($SLURM_ARRAY_TASK_ID))
-output_file=$output_dir/$job_num.root
+output_file=$output_dir/$job_num
 input_file=`sed -n "${job_num}p" < $file_list`
 
 echo "loading " $hadesroot
